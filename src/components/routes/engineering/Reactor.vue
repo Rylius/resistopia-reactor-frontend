3542576cf32359c64139c86ef8388ad63789cc33
Rylius <ryliusk@gmail.com>
2017-05-12 10:55:38 +0200

    Cleared engineering/reactor page

--- src/components/routes/engineering/Reactor.vue
+++ src/components/routes/engineering/Reactor.vue
@@ -1,21 +1,5 @@
 <template>
     <section>
-        <div class="block-group">
-            <div class="block block-group" style="width: 25%;">
-                <div class="block">
-                    matter
-                    <slider :vertical="false"
-                            :value="state['storage-matter'].releasedMatterPerTick"
-                            @update="value => state['storage-matter'].releasedMatterPerTick = value"></slider>
-                </div>
-                <div class="block">
-                    antimatter
-                    <slider :vertical="false"
-                            :value="state['storage-antimatter'].releasedAntimatterPerTick"
-                            @update="value => state['storage-antimatter'].releasedAntimatterPerTick = value"></slider>
-                </div>
-            </div>
-        </div>
     </section>
 </template>
 
