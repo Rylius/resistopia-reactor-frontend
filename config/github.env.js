1f2fb0a238a386f83f0958b34352a22ea700c3cf
Rylius <ryliusk@gmail.com>
2017-07-16 17:52:26 +0200

    Changed Github websocket URL to use WSS

--- config/github.env.js
+++ config/github.env.js
@@ -2,5 +2,5 @@ var merge = require('webpack-merge');
 var prodEnv = require('./prod.env');
 
 module.exports = merge(prodEnv, {
-    RESISTOPIA_BACKEND_WS: '"ws://resistopia-reactor-backend.rylius.de/ws/frontend"',
+    RESISTOPIA_BACKEND_WS: '"wss://resistopia-reactor-backend.rylius.de/ws/frontend"',
 });
