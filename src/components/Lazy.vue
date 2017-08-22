1cb94fc62c71ce7354a68d658f565ff8406dd579
Rylius <ryliusk@gmail.com>
2017-08-22 21:38:08 +0200

    Added lazy loading component

---
+++ src/components/Lazy.vue
@@ -0,0 +1,27 @@
+<template>
+    <div v-if="visible">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'lazy',
+        props: {
+            delay: {
+                default: 10,
+                type: Number,
+            },
+        },
+        data() {
+            return {
+                visible: false,
+            };
+        },
+        mounted() {
+            setTimeout(() => {
+                this.visible = true;
+            }, this.delay);
+        },
+    };
+</script>
